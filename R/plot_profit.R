6ef7148b76c6213ec8aa52f2febae79a65076f76
Dmitrii Izgurskii <izgurskii@gmail.com>
2013-09-04 22:56:20 +0200

    slight mods

--- R/plot_profit.R
+++ R/plot_profit.R
@@ -1,22 +1,27 @@
 #' Bar plot of arbitrage profit for each tick
 #' 
-#' This function produces a bar plot of profit in pips for each tick. The bars 
-#' are arranged on the x-axis according to their timestamp.
+#' This function produces a bar plots of triangular arbitrage profit in pips
+#' for each tick. The bars are arranged on the x-axis according to their timestamp.
 #' 
-#' @param x 'TriArbProfitTable' object or a data frame with similar properties.
+#' @param x 'TriArbProfitTable' object or a data frame with 4 columns: timestamp,
+#' difference between timestamp, profit of first roundtrip, and profit of second 
+#' roundtrip.
+#'
 #' @param separate logical. If FALSE then one device window will show two plots of the two possible 
 #' triangular arbitrage roundtrips. If TRUE, each plot will be opened in new window. 
+#'
 #' @param ... further arguments for the generic 'plot' function. 
+#'
 #' @export
 #'
-plot_profit <- function(x, separate=FALSE, ...) {
+plot_profit <- function(x, separate_plots=FALSE, ...) {
     single_plot <- function(col, title="", sub="", ...) {
-        par(mai=c(1, 1, 1.5, 0.6), bty="l")
+    # convenience function to print a single plot
+        par(mai=c(1, 1, 1.5, 0.6), bty="l") # set inside margin and box type
         # instead of passing x, thereby copying it unnecessarily, we just past column names
         plot(x$timestamp, x[, col], main=title, las=1, type='h', xlab='', ylab='', ...)
         mtext(build_subtitle(col), line=2, cex=0.9)
     }
-
 
     build_subtitle <- function(col) {
         sub <- paste("Ticks: ", format(nrow(x), big.mark=","), ". ", sep="")
@@ -24,14 +29,14 @@ plot_profit <- function(x, separate=FALSE, ...) {
         sub <- paste(sub, "Positive arbitrages: ", num_pos_profit, ". ", sep="")
         start_date <- format(x$timestamp[1], "%Y-%m-%d %H:%M:%S") 
         end_date <- format(rev(x$timestamp)[1], "%Y-%m-%d %H:%M:%S")
-        period <- paste(start_date, " - ", end_date, sep="")
+        period <- paste(start_date, " \u2014 ", end_date, sep="")
         sub <- paste(sub, "Period: ", period, ". ", sep="")
     }
 
-    title_1 <- paste("Profit in pips for roundtrip ", gsub('_', ' - ', names(x)[3]))
-    title_2 <- paste("Profit in pips for roundtrip ", gsub('_', ' - ', names(x)[4]))
+    title_1 <- paste("Pips profit for ", gsub('_', ' - ', names(x)[3]))
+    title_2 <- paste("Pips profit for ", gsub('_', ' - ', names(x)[4]))
     
-    if (separate==FALSE) {
+    if (separate_plots==FALSE) { #
         dev.new(width=11, height=10, xpos=100, ypos=100)
         par(mfrow=c(2, 1), bty='l')
         single_plot(col=3, title=title_1, sub_1, ...)        
